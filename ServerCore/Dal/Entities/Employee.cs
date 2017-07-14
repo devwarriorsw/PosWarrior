ce11e76ef500af235983eea933d2b845f57c619d
Mobile Warrior <mobile.warrior@outlook.com>
2017-07-14 00:49:51 -0500

    Added dal

--- ServerCore/Dal/Entities/Employee.cs
+++ ServerCore/Dal/Entities/Employee.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ServerCore
+namespace ServerCore.Dal.Entities
 {
-    public class Class1
+    class Employee
     {
     }
 }
---
+++ ServerCore/Dal/Factory/DalFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore.Dal.Factory
+{
+    public class DalFactory
+    {
+        private static DalFactory Instance;
+
+        public static DalFactory Get()
+        {
+            if(Instance == null) Instance = new DalFactory();
+            return Instance;
+        }
+    }
+}
